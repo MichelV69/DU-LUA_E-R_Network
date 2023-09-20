a77643b0892ee9722f5e5fc645425717a6df73e6
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2023-09-20 16:14:38 -0300

    skeleton code to implement testing methods

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,24 @@
--- Here's to the crazy ones. The misfits. The rebels. The troublemakers.
+local ER_Net = require('network')
 
--- The code below is just a "Hello World" that dumps the current links and their element classes
 system.print('Debugging Control Unit...')
 for linkName, element in pairs(library.getLinks()) do
   system.print(string.format('Found link `%s` of type `%s`', linkName, element.getClass()))
 end
+
+--- start of play-pen
+for key, data in ipairs(memoryStore.getKeyList()) do
+  system.print(string.format('Found KEY: `%s` which points to `%d` bytes of data.',key, data.length()))
+end
+
+ER_Net.setChannelName("ws2_comms")
+ER_Net.setNodeID("testPoint-01")
+if (ER_Net.prepare() > 0) then do
+  if (ER_Net.poll() > 0) then do
+    to_app = "mining_data"
+    from_node = ER_Net.getNodeID()
+  end
+end
+--- end of play-pen
 
 -- Stops execution
 unit.exit()
